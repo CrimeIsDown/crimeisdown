d7acdc4d9515f99c3be4d753c137520a1f4008bf
Eric Tendian <erictendian@gmail.com>
2014-11-21 19:05:18 -0600

    Fixed menu, styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,20 +3,3 @@
 $(function onload() {
 
 });
-
-YUI({
-    classNamePrefix: 'pure'
-}).use('gallery-sm-menu', function (Y) {
-
-    var horizontalMenu = new Y.Menu({
-        container         : '#main-menu',
-        sourceNode        : '#main-menu-items',
-        orientation       : 'horizontal',
-        hideOnOutsideClick: false,
-        hideOnClick       : false
-    });
-
-    horizontalMenu.render();
-    horizontalMenu.show();
-
-});
